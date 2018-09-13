24d4f69d81b29b1b2cb9acd857a94c9aaef03e20
Eddy De Leon <deleoneddy45@gmail.com>
2018-09-12 18:13:20 -0800

    Part 2 Complete. Added Part 3 to project.

--- part2/funcs_objects/funcs_objects_tests.py
+++ part2/funcs_objects/funcs_objects_tests.py
@@ -1,4 +1,5 @@
 import unittest
+from part2.funcs_objects.funcs_objects import *
 from part2.funcs_objects.objects import *
 
 
@@ -18,7 +19,23 @@ class TestCases(unittest.TestCase):
         self.assertEqual(circle.radius, 2)
         pass
 
-    # Add other testing functions
+    def test_distance_function(self):
+        point1 = Point(1, 1)
+        point2 = Point(1, 1)
+        point3 = Point(0, 0)
+        point4 = Point(0, 5)
+
+        self.assertEqual(distance(point1, point2), 0)
+        self.assertEqual(distance(point3, point4), 5)
+
+    def test_circles_overlap_function(self):
+        circle1 = Circle(Point(0, 0), 5)
+        circle2 = Circle(Point(0, 1), 5)
+        circle3 = Circle(Point(0, 0), 1)
+        circle4 = Circle(Point(5, 5), 1)
+
+        self.assertTrue(circles_overlap(circle1, circle2), True)
+        self.assertFalse(circles_overlap(circle3, circle4), True)
 
 
 # Run the unit tests.
