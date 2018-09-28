e37f7da96ef1005b796d2cfd4e02bd7953e16ce3
eddyD45 <deleoneddy45@gmail.com>
2018-09-27 21:57:20 -0700

    Part 3 now implements the epsilon_equals function. Ready to turn in.

--- part3/object_equality/object_equality_tests.py
+++ part3/object_equality/object_equality_tests.py
@@ -1,5 +1,6 @@
 import unittest
-from part3.object_equality import point
+import utility
+import point
 
 
 class TestCases(unittest.TestCase):
@@ -7,13 +8,19 @@ class TestCases(unittest.TestCase):
         pt = point.Point(1, 2)
         self.assertAlmostEqual(pt.x, 1)
         self.assertAlmostEqual(pt.y, 2)
+        self.assertTrue(utility.epsilon_equal(pt.x, 1))
+        self.assertTrue(utility.epsilon_equal(pt.y, 2))
 
     def test_point_two(self):
         pt = point.Point(-4.7, 19.2)
         self.assertAlmostEqual(pt.x, -4.7)
-        self.assertAlmostEqual(pt.y, 19.2)
+        self.assertTrue(utility.epsilon_equal(pt.x, -4.7))
 
-    def test_equality(self):
+        self.assertAlmostEqual(pt.y, 19.2)
+        self.assertTrue(utility.epsilon_equal(pt.y, 19.2))
+
+
+def test_equality(self):
         point1 = point.Point(3, 2)
         point2 = point.Point(3, 2)
         self.assertEqual(point1, point2)
